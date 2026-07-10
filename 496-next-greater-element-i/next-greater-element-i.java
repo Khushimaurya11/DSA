1aa56e422652bc1c6b0de1071a493bfb8cf52ef2
Khushi Maurya <khushimaurya0527@gmail.com>
2026-07-10 23:44:57 +0530

    Time: 5 ms (16.35%) | Memory: 44.5 MB (94.81%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        Arrays.fill(ans,-1);
+        int count = 0;
+        for(int i = 0; i<nums1.length;i++){
+            for(int j = 0; j<nums2.length;j++){
+                
+                if(nums1[i] == nums2[j]){
+                    for(int k = j+1; k<nums2.length;k++){
+                        if(nums2[k] > nums1[i]){
+                          ans[i] = nums2[k]; 
+                          break;
+                    }
+                    else ans[i] = -1;
+                }
+                }
+            }
+        }
+        return ans;
+    }
+}
