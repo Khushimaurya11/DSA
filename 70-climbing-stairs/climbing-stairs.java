e4fae5739066c66099af424ef782853e3362bb71
Khushi Maurya <khushimaurya0527@gmail.com>
2026-07-11 23:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.11%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+     public int steps(int n,int[] dp) {
+        if(n <= 1) return 1; 
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = steps(n-1,dp)+steps(n-2,dp);
+             }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return steps(n,dp);
+    }
+}
