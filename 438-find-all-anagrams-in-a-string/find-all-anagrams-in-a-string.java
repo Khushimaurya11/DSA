ecd33c9fca3253db23d80dcfc7e579e77c91a34e
Khushi Maurya <khushimaurya0527@gmail.com>
2026-09-04 22:44:04 +0530

    Time: 10 ms (80.95%) | Memory: 46.2 MB (99.29%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,44 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        int sLen = s.length();
-        int pLen = p.length();
-
-        if (pLen > sLen) {
-            return result;
+        int n = s.length();
+        int m = p.length();
+        int i = 0;
+        int j = m;
+        int[] pFreq = new int[26];
+        int[] countWindow = new int[26];
+        List<Integer> res = new ArrayList<>();
+        if(m > n) return res;
+        for (int k = 0; k < m; k++) {
+            pFreq[p.charAt(k) - 'a']++;
+            countWindow[s.charAt(k) - 'a']++;
         }
-
-        int[] pCount = new int[26];
-        int[] sCount = new int[26];
-
-        // Fill initial frequency arrays for the length of p
-        for (int i = 0; i < pLen; i++) {
-            pCount[p.charAt(i) - 'a']++;
-            sCount[s.charAt(i) - 'a']++;
-        }
-
-        // Check the first window
-        if (Arrays.equals(pCount, sCount)) {
-            result.add(0);
-        }
-
-        // Slide the window across s
-        for (int i = pLen; i < sLen; i++) {
-            // Add the new character entering the window
-            sCount[s.charAt(i) - 'a']++;
-            // Remove the character leaving the window
-            sCount[s.charAt(i - pLen) - 'a']--;
-
-            // If frequencies match, record the start index of the current window
-            if (Arrays.equals(pCount, sCount)) {
-                result.add(i - pLen + 1);
+        while (j <= n) {
+            if (Arrays.equals(pFreq, countWindow)) {
+                res.add(i);
             }
+            if(j==n) break;
+                countWindow[s.charAt(i) - 'a']--;
+                i++;
+                countWindow[s.charAt(j) - 'a']++;
+               j++;
         }
 
-        return result;
+        return res;
     }
 }
